85cb0c271a12cb726b556191cc05876be2ccc969
vedmara <vedmara87@gmail.com>
2021-01-29 00:04:04 +0000

    Homework_№3

---
+++ less_3_task_8.py
@@ -0,0 +1,26 @@
+M = 5
+N = 4
+array = [[], [], [], []]
+
+for i in range(N):
+    summa = 0
+    for j in range(M - 1):
+        a = int(input())
+        summa += a
+        array[i].append(a)
+    array[i].append(summa)
+
+for i in range(0, N):
+    print(array[i])
+
+
+
+
+
+
+
+ #       a = int(input())
+  #      sum = sum + a
+   # m[i][3]=sum
+#print(m)
+    
