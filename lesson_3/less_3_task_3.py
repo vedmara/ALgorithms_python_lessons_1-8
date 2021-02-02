679b02f9ea4c002eb67b887f833d1e5e5f2fefa9
vedmara <vedmara87@gmail.com>
2021-02-02 14:10:48 +0000

    chenges

--- lesson_3/less_3_task_3.py
+++ lesson_3/less_3_task_3.py
@@ -6,12 +6,21 @@ MAX_ITEM = 100
 array = [random.randint(MIN_ITEM, MAX_ITEM) for _ in range(SIZE)]
 print(array)
 
-min_pos = 0
-max_pos = 0
-for i in range(1, len(array)):
-    if array[i] < array[min_pos]:
-        min_pos = i
-    if array[i] > array[max_pos]:
-        max_pos = i
-array[min_pos], array[max_pos] = array[max_pos], array[min_pos]
+#min_pos = 0
+#max_pos = 0
+#for i in range(1, len(array)):
+#    if array[i] < array[min_pos]:
+#        min_pos = i
+#    if array[i] > array[max_pos]:
+#        max_pos = i
+#array[min_pos], array[max_pos] = array[max_pos], array[min_pos]
+#print(array)
+
+mx = max(array)
+ix = array.index(mx)
+mn = min(array)
+iy = array.index(mn)
+array[ix] = mn
+array[iy] = mx
 print(array)
+
